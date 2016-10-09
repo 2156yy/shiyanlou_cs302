02959b87f37e8df368a6ce5ac4931ef6431de515
2156yy <yy734526503@qq.com>
2016-10-09 17:24:31 +0800

    06

--- backUp.py
+++ backUp.py
@@ -9,7 +9,7 @@ import time
 def backup():
 	global entry_source
 	global entry_target
-	source = entry_source.get
+	source = entry_source.get()
 	target_dir = entry_target.get()
 	today_dir = target_dir + time.strftime('%Y%m%d')
 	time_dir = time.strftime("%H%M%S")
@@ -31,17 +31,17 @@ root = Tkinter.Tk()
 root.title('BackUp')
 root.geometry("200x200")
 
-lbl_source = Tkinter.Label(root, text='Source')
+lbl_source = Tkinter.Label(root, text='源地址')
 lbl_source.grid(row=0, column=0)
 entry_source = Tkinter.Entry(root)
 entry_source.grid(row=0,column=1)
 
-lbl_target = Tkinter.Label(root, text='Target')
+lbl_target = Tkinter.Label(root, text='目的地址')
 lbl_target.grid(row=1, column=0)
 entry_target = Tkinter.Entry(root)
 entry_target.grid(row=1,column=1)
 
-but_back = Tkinter.Button(root,text='BackUp')
+but_back = Tkinter.Button(root,text='备份')
 but_back.grid(row=3, column= 0)
 but_back["command"] = backup
 
